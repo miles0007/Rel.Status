ae3fa805d063af671f3bd601cf29f6ad73226ec0
miles0007 <63068087+miles0007@users.noreply.github.com>
2020-04-09 10:01:54 +0530

    Update settings.py

--- game/settings.py
+++ game/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '9*e+j-!_@5*^!l)a6hk17+ck(w0jv0)pu=%fdhgw*s5w_@-)v4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['flames-app-miles.herokuapp.com','127.0.0.1']
 
 
 # Application definition
